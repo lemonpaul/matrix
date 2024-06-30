89bef0f962dd9c057e3f7c8d40cb93ee5514f25d
pavel.lamonov <pavel.lamonov@sbps.ru>
2024-06-30 18:20:18 +0300

    Boolean matrix as mappings

--- isotone_mappings.py
+++ isotone_mappings.py
@@ -9,12 +9,12 @@ def as_mapping(matrix):
 
     return function
             
-def get_functions(dimension: int):
+def get_mappings(dimension: int):
     for index in range(2 ** (dimension * dimension)):
         raw = [int(el) for el in bin(index)[2:].zfill(dimension ** 2)]
         matrix = [raw[i*dimension:i*dimension+dimension] for i in range(dimension)]
         yield as_mapping(matrix)
 
 if __name__ == "__main__":
-    for function in get_functions(3):
+    for function in get_mappings(3):
         print(function)
